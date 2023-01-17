861d2dea72021aab3ecffafe0192977dc536f32c
Vu Anh Lam <vuanhlam000@gmail.com>
2023-01-17 20:52:54 +0700

    5.1 using react-icon

--- src/components/Admin/SideBar.js
+++ src/components/Admin/SideBar.js
@@ -1,111 +1,105 @@
 import 'react-pro-sidebar/dist/css/styles.css';
 import { FaTachometerAlt, FaGem, FaList, FaGithub, FaRegLaughWink, FaHeart } from 'react-icons/fa';
 import { ProSidebar, Menu, MenuItem, SubMenu, SidebarHeader, SidebarFooter, SidebarContent } from 'react-pro-sidebar';
-import { DiReact } from 'react-icons/di';
-import { MdDashboard } from 'react-icons/md';
+import { SiReactivex } from 'react-icons/si'
+import { RiDashboard3Fill } from 'react-icons/ri'
+import { MdOutlineFeaturedPlayList } from 'react-icons/md'
+import { BsFillBarChartFill } from 'react-icons/bs'
+
 import { Link } from 'react-router-dom';
 
 import sidebarBg from '~/assets/images/bg2.jpg';
 
 const SideBar = (props) => {
-  const { image, collapsed, toggled, handleToggleSidebar } = props;
-  return (
-    <ProSidebar
-      image={sidebarBg}
-      collapsed={collapsed}
-      toggled={toggled}
-      breakPoint="md"
-      onToggle={handleToggleSidebar}
-    >
-      <SidebarHeader>
-        <div
-          style={{
-            padding: '24px',
-            textTransform: 'uppercase',
-            fontWeight: 'bold',
-            fontSize: 14,
-            letterSpacing: '1px',
-            overflow: 'hidden',
-            textOverflow: 'ellipsis',
-            whiteSpace: 'nowrap',
-          }}
+    const { image, collapsed, toggled, handleToggleSidebar } = props;
+    return (
+        <ProSidebar
+            image={sidebarBg}
+            collapsed={collapsed}
+            toggled={toggled}
+            breakPoint="md"
+            onToggle={handleToggleSidebar}
         >
-          Quizlet
-        </div>
-      </SidebarHeader>
+            <SidebarHeader>
+                <div
+                    style={{
+                        padding: '24px',
+                        textTransform: 'uppercase',
+                        fontWeight: 'bold',
+                        fontSize: 14,
+                        letterSpacing: '1px',
+                        overflow: 'hidden',
+                        textOverflow: 'ellipsis',
+                        whiteSpace: 'nowrap',
+                    }}
+                >
+                    <SiReactivex size={'3em'} color={"00bfff"}/>
+                    <span className='title'>Quizlet Dashboard</span>
+                </div>
+            </SidebarHeader>
 
-      <SidebarContent>
-        <Menu iconShape="circle">
-          <MenuItem
-            icon={<FaTachometerAlt />}
-            suffix={<span className="badge red">New</span>}
-          >
-            {/* {intl.formatMessage({ id: 'dashboard' })} */}
-            dashboard
-          </MenuItem>
-          <MenuItem icon={<FaGem />}>
-            {/* {intl.formatMessage({ id: 'components' })} */}
-            components
-          </MenuItem>
-        </Menu>
-        <Menu iconShape="circle">
-          <SubMenu
-            suffix={<span className="badge yellow">3</span>}
-            // title={intl.formatMessage({ id: 'withSuffix' })}
-            icon={<FaRegLaughWink />}
-          >
-            <MenuItem>1</MenuItem>
-            <MenuItem>2</MenuItem>
-            <MenuItem>3</MenuItem>
-          </SubMenu>
-          {/* <SubMenu
-            prefix={<span className="badge gray">3</span>}
-            title={intl.formatMessage({ id: 'withPrefix' })}
-            icon={<FaHeart />}
-          >
-            <MenuItem>{intl.formatMessage({ id: 'submenu' })} 1</MenuItem>
-            <MenuItem>{intl.formatMessage({ id: 'submenu' })} 2</MenuItem>
-            <MenuItem>{intl.formatMessage({ id: 'submenu' })} 3</MenuItem>
-          </SubMenu> */}
-          {/* <SubMenu title={intl.formatMessage({ id: 'multiLevel' })} icon={<FaList />}>
-            <MenuItem>{intl.formatMessage({ id: 'submenu' })} 1 </MenuItem>
-            <MenuItem>{intl.formatMessage({ id: 'submenu' })} 2 </MenuItem>
-            <SubMenu title={`${intl.formatMessage({ id: 'submenu' })} 3`}>
-              <MenuItem>{intl.formatMessage({ id: 'submenu' })} 3.1 </MenuItem>
-              <MenuItem>{intl.formatMessage({ id: 'submenu' })} 3.2 </MenuItem>
-              <SubMenu title={`${intl.formatMessage({ id: 'submenu' })} 3.3`}>
-                <MenuItem>{intl.formatMessage({ id: 'submenu' })} 3.3.1 </MenuItem>
-                <MenuItem>{intl.formatMessage({ id: 'submenu' })} 3.3.2 </MenuItem>
-                <MenuItem>{intl.formatMessage({ id: 'submenu' })} 3.3.3 </MenuItem>
-              </SubMenu>
-            </SubMenu>
-          </SubMenu> */}
-        </Menu>
-      </SidebarContent>
+            <SidebarContent>
+                <Menu iconShape="circle">
+                    <MenuItem 
+                        icon={<RiDashboard3Fill />}
+                        suffix={<span className="badge red">New</span>}>
+                        {/* {intl.formatMessage({ id: 'dashboard' })} */}
+                        Dashboard
+                    </MenuItem>
+                </Menu>
+                <Menu iconShape="circle">
+                    <SubMenu
+                        // suffix={<span className="badge yellow">3</span>}
+                        // title={intl.formatMessage({ id: 'withSuffix' })}
+                        icon={<MdOutlineFeaturedPlayList />}
+                        title="Features"
+                    >
+                        <MenuItem>User Management</MenuItem>
+                        <MenuItem>Quiz Management</MenuItem>
+                        <MenuItem>Question Management</MenuItem>
+                    </SubMenu>
+                    {/* <SubMenu
+                        prefix={<span className="badge gray">3</span>}
+                        // title={intl.formatMessage({ id: 'withPrefix' })}
+                        icon={<FaHeart />}
+                    >
+                        <MenuItem>1</MenuItem>
+                        <MenuItem>2</MenuItem>
+                        <MenuItem>3</MenuItem>
+                    </SubMenu> */}
+                    {/* <SubMenu icon={<FaList />}>
+                        <MenuItem>1 </MenuItem>
+                        <MenuItem> 2 </MenuItem>
+                        <SubMenu>
+                            <MenuItem> 3.1 </MenuItem>
+                            <MenuItem>3.2 </MenuItem>
+                            <SubMenu title={`$3.3`}>
+                                <MenuItem>3.3.1 </MenuItem>
+                                <MenuItem>3.3.2 </MenuItem>
+                                <MenuItem>3.3.3 </MenuItem>
+                            </SubMenu>
+                        </SubMenu>
+                    </SubMenu> */}
+                </Menu>
+            </SidebarContent>
 
-      <SidebarFooter style={{ textAlign: 'center' }}>
-        <div
-          className="sidebar-btn-wrapper"
-          style={{
-            padding: '20px 24px',
-          }}
-        >
-          <a
-            href="https://github.com/jvlam?tab=repositories"
-            target="_blank"
-            className="sidebar-btn"
-            rel="noopener noreferrer"
-          >
-            <FaGithub />
-            <span style={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden'}}>
-              {/* {intl.formatMessage({ id: 'viewSource' })} */}
-              Quiz-App
-            </span>
-          </a>
-        </div>
-      </SidebarFooter>
-    </ProSidebar>
-  );
+            <SidebarFooter style={{ textAlign: 'center' }}>
+                <div
+                    className="sidebar-btn-wrapper"
+                    style={{
+                        padding: '20px 24px',
+                    }}
+                >
+                    <div
+                        className="sidebar-btn"
+                        rel="noopener noreferrer"
+                    >
+                        <BsFillBarChartFill />
+                    </div>
+                </div>
+            </SidebarFooter>
+        </ProSidebar>
+    );
 };
 
 export default SideBar;
