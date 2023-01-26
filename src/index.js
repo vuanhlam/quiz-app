5ed43dbe77de56d0f9703b34bdd5369455acff07
Vu Anh Lam <vuanhlam000@gmail.com>
2023-01-26 14:35:09 +0700

    23. setup redux

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import reportWebVitals from './reportWebVitals';
 import GlobalStyle from '~/components/GlobalStyles';
 import Layout from './Layout';
 
+import { Provider } from 'react-redux';
+import  store  from '~/redux/store';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <GlobalStyle>
-    <Layout/>
-  </GlobalStyle>,
+  <Provider store={store}>
+    <GlobalStyle>
+      <Layout/>
+    </GlobalStyle>
+  </Provider>
   // </React.StrictMode>
 );
 
