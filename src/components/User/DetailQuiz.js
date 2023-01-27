579f9a37158d0e06c7f1352bdada6a2b79b58db8
Vu Anh Lam <vuanhlam000@gmail.com>
2023-01-27 23:03:04 +0700

    30. Using lodash library processing data from back-end response like a shit

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,20 +1,41 @@
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import _ from 'lodash';
+
 import { getDataQuestion } from '~/services/apiServices';
 
 function DetailQuiz() {
     const params = useParams();
     const quizId = params.id;
-    console.log('check param: ', params);
 
     useEffect(() => {
-        fetchQuestion()
-    }, [quizId])
+        fetchQuestion();
+    }, [quizId]);
 
-    const fetchQuestion = async() => {
+    const fetchQuestion = async () => {
         const res = await getDataQuestion(quizId);
-        console.log(res);
-    }
+        if (res && res.EC === 0) {
+            let raw = res.DT;
+            let data = _.chain(raw)
+                .groupBy('id')
+                .map((value, key) => {
+                    let answers = [];
+                    let questionDescription, image = null;
+
+                    value.forEach((item, index) => {
+                        if(index === 0) {
+                            questionDescription = item.description
+                            image = item.image
+                        }
+                        answers.push(item.answers)
+                    })
+                    console.log('value: ', value ,'key: ', key);
+                    return { questionId: key, answers, questionDescription, image }
+                })
+                .value();
+            console.log(data);
+        }
+    };
 
     return <div className="detail-quiz-container">DetailQuiz</div>;
 }
