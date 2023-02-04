d7733e8e3471a47d92a9eed9039adcf9cd0f8fc1
Vu Anh Lam <vuanhlam000@gmail.com>
2023-02-04 09:40:40 +0700

    44. Assign Quiz for User

--- src/components/Admin/AdminContent/Quiz/AssignQuiz.js
+++ src/components/Admin/AdminContent/Quiz/AssignQuiz.js
@@ -1,13 +1,15 @@
 import Select from 'react-select';
 import { useState, useEffect } from 'react';
-import { getAllQuizForAdmin, getAllUsers } from '~/services/apiServices';
+import { getAllQuizForAdmin, getAllUsers, postAssignQuiz } from '~/services/apiServices';
+import { toast } from 'react-toastify';
+
 
 function AssignQuiz() {
     const [selectedQuiz, setSelectedQuiz] = useState({});
     const [listQuiz, setListQuiz] = useState([]);
 
     const [listUser, setListUser] = useState([]);
-    const [selectedUser, setSelectedUser] = useState({ });
+    const [selectedUser, setSelectedUser] = useState({});
 
     useEffect(() => {
         fetchQuiz();
@@ -20,7 +22,7 @@ function AssignQuiz() {
             let newQuiz = res.DT.map((item) => {
                 return {
                     value: item.id,
-                    label: `${item.id} - ${item.description}`,
+                    label: `${item.id} - ${item.name}`,
                 };
             });
             setListQuiz(newQuiz);
@@ -39,6 +41,16 @@ function AssignQuiz() {
             setListUser(newUsers);
         }
     };
+
+    const handleAssign = async () => {
+      let res = await postAssignQuiz(selectedQuiz.value, selectedUser.value);
+      console.log('res: ', res);
+      if(res && res.EC === 0) {
+        toast.success(res.EM);
+      }else {
+        toast.error(res.EM)
+      }
+    }
 
     return (
         <div className="assign-quiz-wrapper row">
@@ -59,7 +71,12 @@ function AssignQuiz() {
                 />
             </div>
             <div>
-              <button className='btn btn-warning mt-3'>Assign</button>
+              <button 
+                className='btn btn-warning mt-3'
+                onClick={() => handleAssign()}
+              >
+                Assign
+              </button>
             </div>
         </div>
     );
